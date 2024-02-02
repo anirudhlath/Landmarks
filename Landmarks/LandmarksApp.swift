f95efa30843d6c7b22d561b5b894ac7c5a3f6f7b
Anirudh Lath <anirudhlath@gmail.com>
2024-02-01 20:49:06 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Anirudh Lath on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
